d66abef07ed0929d37e9e28b956df0f41a394a57
arthur-machado <arthurmachado7@gmail.com>
2021-08-01 16:03:51 -0300

    feat: added user context to App.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-
-import Routes from './routes'
-import './stylesheets/global.css'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { UserContextProvider } from './hooks/UserContext';
+import Routes from './routes';
+import './stylesheets/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes />
-    </div>
-  )
+    <UserContextProvider>
+      <div className="App">
+        <Navbar />
+        <Routes />
+      </div>
+    </UserContextProvider>
+  );
 }
 
-export default App
+export default App;
